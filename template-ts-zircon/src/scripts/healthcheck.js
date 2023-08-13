33f5c0464d663f52543d1feb03dceb43657b19de
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2023-08-13 11:14:24 -0500

    add healthcheck script

---
+++ template-ts-zircon/src/scripts/healthcheck.js
@@ -0,0 +1,11 @@
+const { execSync } = require("child_process");
+
+try {
+    // Check if the global CLI tool is installed
+    execSync("sern --version");
+    
+    console.log("HEALTH: sern cli is installed.");
+} catch (error) {
+    console.log("HEALTH: sern cli tool not found");
+    console.log("Please install our cli with npm install -g @sern/cli");
+}
